b087c46b716a6e8146b04745dc6f90152f4123d0
Vinayak-HUB1 <dumbre.vinayak@gmail.com>
2022-06-26 08:17:27 +0530

    update 0.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PROJECT_NAME = "oneNeuron-pkg"
 
 setuptools.setup(
     name=f"{PKG_NAME}-{USER_NAME}",
-    version="0.0.3",
+    version="0.0.4",
     author=USER_NAME,
     author_email="dumbre.vinayak@gmail.com",
     description="A small package for perceptron",
